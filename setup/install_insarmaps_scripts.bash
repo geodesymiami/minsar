18d673c1bbb5125cddf49f7b27db70fd69710456
Falk Amelung <famelung@earth.miami.edu>
2025-06-11 11:40:13 -0400

    install_insarmaps_scripts.bash: new script

--- setup/install_insarmaps_scripts.bash
+++ setup/install_insarmaps_scripts.bash
@@ -1,23 +1,9 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -eox pipefail
 
-### Source the environment  #################
 export MINSAR_HOME=$PWD
-
 source setup/platforms_defaults.bash;
 source setup/environment.bash;
-
-
-### git clone the code   #################
 git clone git@github.com:geodesymiami/insarmaps_scripts.git tools/insarmaps_scripts
-git clone git@github.com:geodesymiami/insarmaps.git tools/insarmaps
-
-mamba install --file tools/insarmaps_scripts/conda_requirements.txt --yes -c conda-forge
-pip install -r tools/insarmaps_scripts/pip_requirements.txt
-
-###  Reduce miniforge3 directory size #################
-rm -rf tools/miniforge3/pkgs
-
-echo ""
-echo "Installation of install_insarmaps_scripts.bash DONE"
-echo ""
+mamba install tippecanoe mintpy --yes -c conda-forge
+pip install psycopg2 pycurl
