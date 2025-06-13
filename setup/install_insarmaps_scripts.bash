800f86765f4c1a6db454f1c498c6675aede803db
Falk Amelung <famelung@earth.miami.edu>
2025-06-13 17:12:26 -0400

    updated

--- setup/install_insarmaps_scripts.bash
+++ setup/install_insarmaps_scripts.bash
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-set -eox pipefail
+set -eo pipefail
 
 export MINSAR_HOME=$PWD
 source setup/platforms_defaults.bash;
 source setup/environment.bash;
 [[ -d tools/insarmaps_scripts ]] || \
    git clone git@github.com:geodesymiami/insarmaps_scripts.git tools/insarmaps_scripts
+
+conda create --name insarmaps_scripts pip -y
+source tools/miniforge3/etc/profile.d/conda.sh
+conda activate insarmaps_scripts
+
 mamba install tippecanoe mintpy --yes -c conda-forge
 pip install psycopg2 pycurl geocoder
