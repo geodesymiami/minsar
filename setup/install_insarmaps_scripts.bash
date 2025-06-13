016911bef04ba81b85b0ceebc243bf1773464036
Falk Amelung <famelung@earth.miami.edu>
2025-06-13 19:48:49 -0400

    fixed

--- setup/install_insarmaps_scripts.bash
+++ setup/install_insarmaps_scripts.bash
@@ -7,7 +7,7 @@ source setup/environment.bash;
 [[ -d tools/insarmaps_scripts ]] || \
    git clone git@github.com:geodesymiami/insarmaps_scripts.git tools/insarmaps_scripts
 
-conda create --name insarmaps_scripts pip -y
+conda create --name insarmaps_scripts python=3.10 pip -y
 source tools/miniforge3/etc/profile.d/conda.sh
 conda activate insarmaps_scripts
 
