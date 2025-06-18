2d017faa314bb8047f7aec2b6e31d2e06f40dc27
Falk Amelung <famelung@earth.miami.edu>
2025-06-18 19:09:49 -0400

    subset_sarvey.bash: reverted to use Falks old MintPy subset PR

--- minsar/utils/subset_sarvey.bash
+++ minsar/utils/subset_sarvey.bash
@@ -2,20 +2,18 @@
 set -e
 
 function print_help {
-    echo "Usage: subset_sarvey.bash MIAPLPY_DIR [--lat LAT_MIN LAT_MAX --lon LON_MIN LON_MAX] [--outdir OUTPUT_DIR]"
+    echo "Usage: subset_sarvey.bash MIAPLPY_DIR [--lat LAT_MIN LAT_MAX --lon LON_MIN LON_MAX] [--outdir OUTDIR]"
     echo "       or"
-    echo "       subset_sarvey.bash MIAPLPY_DIR LAT_MIN:LAT_MAX,LON_MIN:LON_MAX --outdir OUTPUT_DIR"
-    echo "           use --no-latest-mintpy-version for "
+    echo "       subset_sarvey.bash MIAPLPY_DIR LAT_MIN:LAT_MAX,LON_MIN:LON_MAX --outdir OUTDIR"
+    echo "           use --no-latest-mintpy-version for using"
     echo "           Falk's  MintPy modification supporting subsetting of non-geocoded data: "
     echo "           https://github.com/falkamelung/MintPy"
-    echo "           (in Miami using alias setmintpyfalk)"
     echo ""
     echo "Examples:"
     echo "  subset_sarvey.bash miaplpy_MDCBeach_201710_202310 --lat 25.937 25.958 --lon -80.125 -80.118 --outdir Oceana"
     echo "  subset_sarvey.bash miaplpy_MDCBeach_201710_202310 25.937:25.958,-80.125:-80.118 --outdir Oceana"
     echo ""
     echo "  subset_sarvey.bash miaplpy_SN_201606_201608 --lat -0.84 -0.83 --lon -91.17 -91.16 --outdir minas"
-    echo "  subset_sarvey.bash miaplpy_SN_201606_201608 --lat -0.84 -0.83 --lon -91.17 -91.16 --outdir minas --no-latest-mintpy-version"
     exit 0
 }
 
@@ -31,8 +29,6 @@ LAT_MAX=""
 LON_MIN=""
 LON_MAX=""
 OUTDIR=""
-
-latest_mintpy_version_flag=1
 
 # Parse bbox shorthand
 if [[ "$1" =~ ^[0-9.+-]+:[0-9.+-]+,[-0-9.]+:[-0-9.]+$ ]]; then
@@ -61,10 +57,6 @@ else
             --outdir)
                 OUTDIR="$2"
                 shift 2
-                ;;
-            --no-latest-mintpy-version)
-                latest_mintpy_version_flag=0
-                shift 1
                 ;;
             *)
                 echo "Unknown option: $1"
@@ -95,6 +87,7 @@ else
     # OUTDIR=../$OUTDIR
     mkdir -p "$OUTDIR/inputs"
     echo "$(date +'%Y-%m-%d %H:%M:%S')  subset_sarvey.bash $INPUT_DIR --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX --outdir $OUTDIR " >> "${OUTDIR}/subset_sarvey.log"
+    cp "${OUTDIR}/subset_sarvey.log" ${OUTDIR}/inputs
 
     echo "cd into $BASE_DIR/inputs ..."
     cd "$BASE_DIR/inputs"
@@ -103,28 +96,42 @@ fi
 [[ -f "slcStack.h5" ]] || { echo "Missing slcStack.h5"; exit 1; }
 [[ -f "geometryRadar.h5" ]] || { echo "Missing geometryRadar.h5"; exit 1; }
 
-
+latest_mintpy_version_flag=0
 if [[ $latest_mintpy_version_flag == "1" ]]; then
-   echo "Running... subset.py slcStack.h5 geometryRadar.h5 --lookup geometryRadar.h5 --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX"
-   echo "mv sub_slcStack.h5 sub_geometryRadar.h5 ../$OUTDIR/inputs/slcStack.h5"
-   subset.py slcStack.h5 geometryRadar.h5  --lookup geometryRadar.h5 --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX
-   mv "sub_slcStack.h5" "../../$OUTDIR/inputs/slcStack.h5"
-   mv "sub_geometryRadar.h5" "../../$OUTDIR/inputs/geometryRadar.h5"
+    echo "Running... subset.py slcStack.h5 geometryRadar.h5 --lookup geometryRadar.h5 --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX"
+    echo "mv sub_slcStack.h5 sub_geometryRadar.h5 ../$OUTDIR/inputs/slcStack.h5"
+    subset.py slcStack.h5 geometryRadar.h5  --lookup geometryRadar.h5 --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX
+    mv "sub_slcStack.h5" "../../$OUTDIR/inputs/slcStack.h5"
+    mv "sub_geometryRadar.h5" "../../$OUTDIR/inputs/geometryRadar.h5"
 else
-   ### --no-latest-mintpy-version  (uses Falk's version)
-   echo "Running... subset.py $INPUT_DIR/slcStack.h5 --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX --output $OUTDIR/inputs/slcStack.h5"
-   subset.py $INPUT_DIR/slcStack.h5 --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX --output $OUTDIR/inputs/slcStack.h5
-   echo "Running... subset.py $INPUT_DIR/geometryRadar.h5 --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX --output $OUTDIR/inputs/geometryRadar.h5"
-   subset.py "$INPUT_DIR/geometryRadar.h5" --lat "$LAT_MIN" "$LAT_MAX" --lon "$LON_MIN" "$LON_MAX" --output "$OUTDIR/inputs/geometryRadar.h5"
+    ### --no-latest-mintpy-version  (uses Falk's version)
+    export MINTPY_HOME="${RSMASINSAR_HOME}/tools/MintPy_falk"
+    export PATH="${MINTPY_HOME}/src/mintpy/cli:$PATH"
+    export PYTHONPATH="${MINTPY_HOME}/src:${PYTHONPATH}"
+    export DASK_CONFIG=$(echo "$DASK_CONFIG" | sed 's|MintPy|MintPy_falk|g')
+   
+    echo "Current working dir: $PWD"
 
-   PHASE_FILE="inverted/phase_series.h5"
-   if [[ -f "$PHASE_FILE" ]]; then
-       mkdir -p "$OUTDIR/inverted"
-       echo "Running... subset.py $PHASE_FILE --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX --output $OUTDIR/inverted/phase_series.h5"
-       subset.py "$PHASE_FILE" --lat "$LAT_MIN" "$LAT_MAX" --lon "$LON_MIN" "$LON_MAX" --output "$OUTDIR/inverted/phase_series.h5"
-   else
-       echo "inverted/phase_series.h5 does not exist, can’t subset"
-   fi
+    echo "Running....... subset.py slcStack.h5 --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX"
+    echo "followed by... mv sub_slcStack.h5 ../../${OUTDIR}/inputs/slcStack.h5"
+    echo ""
+    subset.py slcStack.h5 --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX
+    mv "sub_slcStack.h5" "../../${OUTDIR}/inputs/slcStack.h5"
+    
+    echo "Running... subset.py geometryRadar.h5 --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX"
+    echo "followed by... mv sub_geometryRadar.h5 ../../${OUTDIR}/inputs/geometryRadar.h5"
+    echo ""
+    subset.py "geometryRadar.h5" --lat "$LAT_MIN" "$LAT_MAX" --lon "$LON_MIN" "$LON_MAX"
+    mv "sub_geometryRadar.h5" "../../${OUTDIR}/inputs/geometryRadar.h5"
 
+    #PHASE_FILE="inverted/phase_series.h5"
+    #if [[ -f "$PHASE_FILE" ]]; then
+    #    mkdir -p "$OUTDIR/inverted"
+    #    echo "Running... subset.py $PHASE_FILE --lat $LAT_MIN $LAT_MAX --lon $LON_MIN $LON_MAX --output $OUTDIR/inverted/phase_series.h5"
+    #    subset.py "$PHASE_FILE" --lat "$LAT_MIN" "$LAT_MAX" --lon "$LON_MIN" "$LON_MAX" --output "$OUTDIR/inverted/phase_series.h5"
+    #else
+    #    echo "inverted/phase_series.h5 does not exist, can’t subset"
+    #fi
 fi
- cd - >/dev/null
+cd - 
+echo "sarvey_subset.bash completed. Output files in $OUTDIR/inputs"
