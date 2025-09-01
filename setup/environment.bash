9989a749108bbbcc6abccce9fe5b67a3883b6f2d
falkamelung <famelung@earth.miami.edu>
2025-09-01 11:33:38 -0500

    environment.bash: added scripts dir

--- setup/environment.bash
+++ setup/environment.bash
@@ -106,6 +106,7 @@ export PATH=${PATH}:${MINSAR_HOME}/minsar/src/minsar/cli
 export PATH=${PATH}:${MINSAR_HOME}/minsar
 export PATH=${PATH}:${MINSAR_HOME}/minsar/insarmaps_utils
 export PATH=${PATH}:${MINSAR_HOME}/minsar/utils
+export PATH=${PATH}:${MINSAR_HOME}/minsar/scripts
 export PATH=${PATH}:${MINTPY_HOME}/src/mintpy/legacy         # for add_attribute.py
 export PATH=${PATH}:${MIAPLPY_HOME}/src/miaplpy
 export PATH=${PATH}:${PLOTDATA_HOME}/src/plotdata/cli
