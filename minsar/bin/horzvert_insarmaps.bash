42b70ec60b3cbfd11adb0e1893640494abc981bb
Falk Amelung <famelung@earth.miami.edu>
2025-12-05 10:59:30 -0500

    horzvert_insarmaps.bash: simplified examples

--- minsar/bin/horzvert_insarmaps.bash
+++ minsar/bin/horzvert_insarmaps.bash
@@ -15,15 +15,15 @@ source ${SCRIPT_DIR}/../lib/utils.sh
 
 if [[ "$1" == "--help" || "$1" == "-h" ]]; then
     helptext="
-  Examples:
-      $SCRIPT_NAME file1/mintpy file2/mintpy --ref-lalo 19.462,-154.870
-      $SCRIPT_NAME file1/mintpy file2/mintpy --ref-lalo 0.84969 -77.86430 --start-date 20250101 --end-date 20251031
-      $SCRIPT_NAME ChilesSenAT120/mintpy ChilesSenDT142/mintpy --ref-lalo 0.84969 -77.86430 --intervals 6
-      $SCRIPT_NAME hvGalapagosSenA106/mintpy hvGalapagosSenD128/mintpy -ref-lalo -0.81,-91.190
-      $SCRIPT_NAME hvGalapagosSenA106/mintpy hvGalapagosSenD128/mintpy -ref-lalo -0.81,-91.190 --intervals 4
-      $SCRIPT_NAME hvGalapagosSenA106/miaplpy_SN_201803_201805/network_single_reference hvGalapagosSenD128/miaplpy_SN_201803_201806/network_single_reference --ref-lalo -0.81 -91.190
-      $SCRIPT_NAME hvGalapagosSenA106/miaplpy/network_single_reference hvGalapagosSenD128/miaplpy/network_single_reference --ref-lalo -0.81 -91.190
-      $SCRIPT_NAME MaunaLoaSenDT87/mintpy MaunaLoaSenAT124/mintpy --period 20181001:20191031 --ref-lalo 19.50068 -155.55856 --ref-lalo -0.81 -91.190
+Examples:
+    $SCRIPT_NAME ChilesSenD142/mintpy ChilesSenA120/mintpy --ref-lalo 0.84969 -77.86430
+    $SCRIPT_NAME ChilesSenD142/mintpy ChilesSenA120/mintpy --ref-lalo 0.84969 -77.86430 --dry-run
+    $SCRIPT_NAME ChilesSenD142/mintpy ChilesSenA120/mintpy --ref-lalo 0.84969 -77.86430 --intervals 6
+    $SCRIPT_NAME hvGalapagosSenD128/mintpy hvGalapagosSenA106/mintpy --ref-lalo -0.81 -91.190
+    $SCRIPT_NAME hvGalapagosSenD128/miaplpy_SN_201803_201806/network_single_reference hvGalapagosSenA106/miaplpy_SN_201803_201805/network_single_reference--ref-lalo -0.81 -91.190
+    $SCRIPT_NAME FernandinaSenD128/mintpy/ FernandinaSenA106/mintpy/ --ref-lalo -0.453 -91.390
+    $SCRIPT_NAME FernandinaSenD128/miaplpy/network_delaunay_4 FernandinaSenA106/miaplpy/network_delaunay_4 --ref-lalo -0.453 -91.390
+    $SCRIPT_NAME MaunaLoaSenDT87/mintpy MaunaLoaSenAT124/mintpy --period 20181001:20191031 --ref-lalo 19.50068 -155.55856 --ref-lalo -0.81 -91.190
 
   Options:
       --mask-thresh FLOAT             Coherence threshold for masking (default: 0.55)
