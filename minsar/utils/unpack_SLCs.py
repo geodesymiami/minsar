b4bb04c581d56469a1c1a5793353a82586217fc3
falkamelung <famelung@earth.miami.edu>
2026-01-09 21:46:49 -0600

    unpack_SLCs.py: new script for non-Sentinel-1 data

--- minsar/utils/unpack_SLCs.py
+++ minsar/utils/unpack_SLCs.py
@@ -18,94 +18,6 @@ from minsar.objects.unpack_sensors import Sensors
 
 pathObj = PathFind()
 
-###############################################
-def convert_subset_lalo_to_boundingBox_string(subset_lalo, delta_lat, delta_lon):
-    """ Converts a subset.lalo string in SNEW format (e.g. "2.7:2.8,125.3:125.4") to a boundingBox string."""
-
-    lat_string = subset_lalo.split(',')[0]
-    lon_string = subset_lalo.split(',')[1]
-
-    min_lat = float(lat_string.split(':')[0]) - delta_lat
-    max_lat = float(lat_string.split(':')[1]) + delta_lat
-    min_lon = float(lon_string.split(':')[0]) - delta_lon
-    max_lon = float(lon_string.split(':')[1]) + delta_lon
-
-    boundingBox_string = '{:.2f} {:.2f} {:.2f} {:.2f}'.format(min_lat, max_lat, min_lon, max_lon)
-
-    return boundingBox_string
-
-###############################################
-def convert_intersectsWith_string_to_boundingBox_string(intersectsWith_str, delta_lat, delta_lon):
-    """ converts polygon string of the form:
-        POLYGON((-86.581 12.3995,-86.4958 12.3995,-86.4958 12.454,-86.581 12.454,-86.581 12.3995))
-        48.1153435942954,32.48224314182711,0 48.1460783620229,32.49847964019297,0 48.1153435942954,32.48224314182711,0
-           same functions convert_polygon_str() in convert_polygon_string.py, should move into ustilities
-    """
-    longs = []
-    lats = []
-
-    if "POLYGON" in intersectsWith_str:
-        modified_str = intersectsWith_str.removeprefix('POLYGON((')
-        modified_str = modified_str.removesuffix('))')
-
-        points = modified_str.split(',')
-
-        # Split each coordinate point to get longitude and latitude
-        for point in points:
-            long, lat = point.split()
-            longs.append(float(long))
-            lats.append(float(lat))
-    else:
-        points = intersectsWith_str.split(' ')
-        for point in points:
-            long, lat, z = point.split(',')
-            longs.append(float(long))
-            lats.append(float(lat))
-
-    min_lat = min(lats)
-    max_lat = max(lats)
-    min_lon = min(longs)
-    max_lon = max(longs)
-
-    min_lat = round(min_lat,3)
-    max_lat = round(max_lat,3)
-    min_lon = round(min_lon,3)
-    max_lon = round(max_lon,3)
-
-    min_lat_bbox = round(min_lat - delta_lat,1)
-    max_lat_bbox = round(max_lat + delta_lat,1)
-    min_lon_bbox = round(min_lon - delta_lon,1)
-    max_lon_bbox = round(max_lon + delta_lon,1)
-
-    bbox_str = str(min_lat_bbox) + ' ' + str(max_lat_bbox) + ' ' + str(min_lon_bbox) + ' ' + str(max_lon_bbox)
-    subset_str = str(min_lat) + ':' + str(max_lat) + ',' + str(min_lon) + ':' + str(max_lon)
-
-    tops_stack_bbox_str = 'topsStack.boundingBox                = ' + bbox_str
-
-    mintpy_subset_str  = 'mintpy.subset.lalo                   = ' + subset_str + '    #[S:N,W:E / no], auto for no'
-    miaplpy_subset_str = 'miaplpy.subset.lalo                  = ' + subset_str + '    #[S:N,W:E / no], auto for no'
-
-    #print("Desired strings: ")
-    #print(tops_stack_bbox_str)
-    #print(mintpy_subset_str)
-    #print(miaplpy_subset_str)
-    return bbox_str
-###########################################################################################
-def get_bbox_from_template(inps, delta_lat, delta_lon):
-    """generates boundingBox string from mintpy.subset.lalo, miaplpy.subset.lalo or intersectsWith POLYGON string"""
-
-    if 'mintpy.subset.lalo' in inps.template.keys():
-        print(f"Creating topsStack.boundingBox using mintpy.subset.lalo={inps.template['mintpy.subset.lalo']} (with delta_lat, delta_lon: {delta_lat}, {delta_lon}) ")
-        boundingBox_string = convert_subset_lalo_to_boundingBox_string(inps.template['mintpy.subset.lalo'], delta_lat, delta_lon)
-    elif 'miaplpy.subset.lalo' in inps.template.keys():
-        print(f"Creating topsStack.boundingBox using miaplpy.subset.lalo={inps.template['miaplpy.subset.lalo']} (with delta_lat, delta_lon: {delta_lat}, {delta_lon})")
-        boundingBox_string = convert_subset_lalo_to_boundingBox_string(inps.template['miaplpy.subset.lalo'], delta_lat, delta_lon)
-    else:
-        print("Creating topsStack.boundingBox using ssaraopt.intersectsWith={inps.template['ssaraopt.intersectsWith']} (with delta_lat, delta_lon: {delta_lat}, {delta_lon})")
-        boundingBox_string = convert_intersectsWith_string_to_boundingBox_string(inps.template['ssaraopt.intersectsWith'], delta_lat, delta_lon)
-
-    return boundingBox_string
-
 ###########################################################################################
 def main(iargs=None):
     inps = putils.cmd_line_parse(iargs, script='create_runfiles')
@@ -118,6 +30,45 @@ def main(iargs=None):
     message_rsmas.log(inps.work_dir, os.path.basename(__file__) + ' ' + ' '.join(input_arguments))
 
     os.chdir(inps.work_dir)
+
+    slc_dir = inps.template[inps.prefix + 'Stack.slcDir']
+    os.makedirs(slc_dir, exist_ok=True)
+
+    # infer platform if it is missing
+    platform = inps.template.get('ssaraopt.platform')
+    if platform == 'None':
+       inps.template['ssaraopt.platform'] = None
+    if inps.template.get('ssaraopt.platform') is None:
+        collection = inps.template.get('ssaraopt.collectionName', '')
+        if 'CSK' in collection:
+            inps.template['ssaraopt.platform'] = 'COSMO_SKYMED'
+        elif 'TSX' in collection:
+            inps.template['ssaraopt.platform'] = 'TERRASAR-X'
+
+    if inps.template.get('ssaraopt.platform') in ("TERRASAR-X", "COSMO_SKYMED"):
+        # Unpack Raw data:
+        if not inps.template['raw_image_dir'] in [None, 'None']:
+            #raw_image_dir = inps.template['raw_image_dir']               # FA 1/23: it would be better to have ORIG_DATA set in defaults for both CSK and TSX
+            raw_image_dir = os.path.join(inps.work_dir, inps.template['raw_image_dir'])
+        else:
+            raw_image_dir = os.path.join(inps.work_dir, 'RAW_data')
+
+        if os.path.exists(raw_image_dir):
+            unpackObj = Sensors(raw_image_dir, slc_dir, remove_file='False',
+                                multiple_raw_frame=inps.template['multiple_raw_frame'])
+            unpack_run_file = unpackObj.start()
+            unpackObj.close()
+
+            inps.out_dir = inps.work_dir
+            inps.num_data = 1
+            job_obj = JOB_SUBMIT(inps)  
+            job_obj.write_batch_jobs(batch_file=unpack_run_file)
+            job_status = job_obj.submit_batch_jobs(batch_file=unpack_run_file)
+
+            if not job_status:
+                raise Exception('ERROR: Unpacking was failed')
+        else:
+            raise Exception('ERROR: No data (SLC or Raw) available')
 
     # for further processing set inps.template['topsStack.demDir']
     if inps.template[inps.prefix + 'Stack.demDir'] == 'None':
@@ -159,8 +110,6 @@ def main(iargs=None):
             shutil.rmtree(inps.work_dir + '/tmp_coreg_secondarys', ignore_errors=True)
             shutil.move(inps.work_dir + '/coreg_secondarys', inps.work_dir + '/tmp_coreg_secondarys' ) 
 
-    inps.num_data = 1
-    job_obj = JOB_SUBMIT(inps)
     runObj = CreateRun(inps)
     runObj.run_stack_workflow()
 
