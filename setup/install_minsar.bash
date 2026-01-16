71a5c180a416b586d3f0a9a29efdad9385a0a968
Falk Amelung <famelung@earth.miami.edu>
2026-01-15 20:59:38 -0500

    setup/install_minsar.bash: for non-committed files replaced copying by ln -sf

--- setup/install_minsar.bash
+++ setup/install_minsar.bash
@@ -32,7 +32,6 @@ if [[ "$(uname)" == "Darwin" ]]; then
     sed -i '' '/#- pymaxflow/ s/#- pymaxflow/- pymaxflow/' minsar_env_MacOS.yml   # activate pip pymaxflow installation
 fi
 
-
 if [[ "$(uname)" == "Linux" ]]; then
     if [[ -f conda-lock.yml ]]; then
        echo "Lock file conda-lock.yml found. Using it for installation"
@@ -64,9 +63,10 @@ cc=tools/miniforge3/bin/cc
 make -C tools/snaphu-v2.0.5/src
 
 ### Adding not-commited MintPy fixes
-ln -sf $PWD/additions/mintpy/plot_network.py $PWD/tools/MintPy/src/mintpy
-ln -sf $PWD/additions/mintpy/save_hdfeos5.py $PWD/tools/MintPy/src/mintpy
-ln -sf $PWD/additions/mintpy/cli/save_hdfeos5.py $PWD/tools/MintPy/src/mintpy/cli
+MINSAR_HOME="$(pwd)"
+ln -sf $MINSAR_HOME/additions/mintpy/plot_network.py $MINSAR_HOME/tools/MintPy/src/mintpy
+ln -sf $MINSAR_HOME/additions/mintpy/save_hdfeos5.py $MINSAR_HOME/tools/MintPy/src/mintpy
+ln -sf $MINSAR_HOME/additions/mintpy/cli/save_hdfeos5.py $MINSAR_HOME/tools/MintPy/src/mintpy/cli
 #ln -sf $MINSAR_HOME/additions/mintpy/plot_network.py $MINTPY_HOME/src/mintpy
 #ln -sf $MINSAR_HOME/additions/mintpy/save_hdfeos5.py $MINTPY_HOME/src/mintpy
 #ln -sf $MINSAR_HOME/additions/mintpy/cli/save_hdfeos5.py $MINTPY_HOME/src/mintpy/cli
@@ -75,28 +75,20 @@ ln -sf $PWD/additions/mintpy/cli/save_hdfeos5.py $PWD/tools/MintPy/src/mintpy/cl
 #cp -p additions/mintpy/cli/save_hdfeos5.py tools/MintPy/src/mintpy/cli/
 
 ### Adding not-committed MiaplPy fixes (for the first Sara said she will do it; the second is wrongly out-commented isce imports)
-cp -p additions/miaplpy/prep_slc_isce.py tools/MiaplPy/src/miaplpy
-cp additions/miaplpy/unwrap_ifgram.py tools/MiaplPy/src/miaplpy
-cp additions/miaplpy/utils.py tools/MiaplPy/src/miaplpy/objects
+ln -sf $MINSAR_HOME/additions/miaplpy/prep_slc_isce.py $MINSAR_HOME/tools/MiaplPy/src/miaplpy
+ln -sf $MINSAR_HOME/additions/miaplpy/unwrap_ifgram.py $MINSAR_HOME/tools/MiaplPy/src/miaplpy
+ln -sf $MINSAR_HOME/additions/miaplpy/utils.py $MINSAR_HOME/tools/MiaplPy/src/miaplpy/objects
 
 ### Adding ISCE fixes and copying checked-out ISCE version (the latest) into miniforge directory ###
 if [[ "$(uname)" == "Linux" ]]; then
 :
 ##cp -p additions/isce/logging.conf tools/miniforge3/envs/minsar/lib/python3.10/site-packages/isce/defaults/logging
-cp -p additions/isce2/contrib/stack/topsStack/FilterAndCoherence.py tools/isce2/contrib/stack/topsStack
-cp -p additions/isce2/contrib/stack/stripmapStack/prepRawCSK.py tools/isce2/contrib/stack/stripmapStack
-cp -p additions/isce2/contrib/stack/stripmapStack/unpackFrame_TSX.py tools/isce2/contrib/stack/stripmapStack
-cp -p additions/isce2/contrib/demUtils/demstitcher/DemStitcher.py tools/isce2/contrib/demUtils/demstitcher
-cp -p additions/isce2/components/isceobj/Sensor/TOPS/Sentinel1.py tools/isce2/components/isceobj/Sensor/TOPS
-
-cp -p additions/isce2/components/isceobj/Sensor/TOPS/Sentinel1.py tools/miniforge3/envs/minsar/lib/python3.10/site-packages/isce/components/isceobj/Sensor/TOPS/Sentinel1.py
-
-### Copying ISCE fixes into miniforge directory ###
-cp -r tools/isce2/contrib/stack/* tools/miniforge3/envs/minsar/share/isce2
-cp -r tools/isce2/components/isceobj/Sensor/TOPS tools/miniforge3/envs/minsar/share/isce2
-cp tools/isce2/components/isceobj/Sensor/TOPS/TOPSSwathSLCProduct.py tools/miniforge3/envs/minsar/lib/python3.??/site-packages/isce/components/isceobj/Sensor/TOPS
-cp tools/isce2/contrib/demUtils/demstitcher/DemStitcher.py  tools/miniforge3/envs/minsar/lib/python3.??/site-packages/isce/components/contrib/demUtils
-
+ln -sf $MINSAR_HOME/additions/isce2/contrib/stack/topsStack/FilterAndCoherence.py $MINSAR_HOME/tools/isce2/contrib/stack/topsStack
+ln -sf $MINSAR_HOME/additions/isce2/contrib/stack/stripmapStack/prepRawCSK.py $MINSAR_HOME/tools/isce2/contrib/stack/stripmapStack
+ln -sf $MINSAR_HOME/additions/isce2/contrib/stack/stripmapStack/unpackFrame_TSX.py $MINSAR_HOME/tools/isce2/contrib/stack/stripmapStack
+ln -sf $MINSAR_HOME/additions/isce2/contrib/demUtils/demstitcher/DemStitcher.py $MINSAR_HOME/tools/isce2/contrib/demUtils/demstitcher
+ln -sf $MINSAR_HOME/additions/isce2/components/isceobj/Sensor/TOPS/Sentinel1.py $MINSAR_HOME/tools/isce2/components/isceobj/Sensor/TOPS
+ln -sf $MINSAR_HOME/additions/isce2/components/isceobj/Sensor/TOPS/Sentinel1.py $MINSAR_HOME/tools/miniforge3/envs/minsar/lib/python3.10/site-packages/isce/components/isceobj/Sensor/TOPS
 #FA 1/2026: this should be done for all modification and remove copying tools/isce2/contrib/stack/* into share/isce2
 ln -sf $MINSAR_HOME/additions/isce2/contrib/stack/stripmapStack/unpackFrame_ENV_raw.py $MINSAR_HOME/tools/miniforge3/envs/minsar/share/isce2/stripmapStack
 ln -sf $MINSAR_HOME/additions/isce2/contrib/stack/stripmapStack/unpackFrame_ENV.py $MINSAR_HOME/tools/miniforge3/envs/minsar/share/isce2/stripmapStack
